cc7fa4b742f6dca631118faa70cca24f95d6a374
lkbhas <lbhasemeyer@gmail.com>
2019-01-27 14:45:12 -0700

    initial commit to get site working

---
+++ src/components/EverythingButEuroTrip.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import MenAtWork from '../assets/images/MenAtWork.jpeg';
+
+const EverythingButEuroTrip = ({
+
+}) => (
+ 	<div>
+  		<img src={MenAtWork} max-height="400" width="100%" />
+	</div>
+);
+
+EverythingButEuroTrip.propTypes = {
+  // count: PropTypes.number,
+  // handleDecrement: PropTypes.func.isRequired,
+  // handleIncrement: PropTypes.func.isRequired,
+};
+
+export default EverythingButEuroTrip;
+
+
+
+// <img src={Diamond} />
